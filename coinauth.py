c5dbf1a2822bd67ce72190bf519d86f38a7f6555
Daniel Rice <drice@greenmangosystems.com>
2014-04-22 23:40:55 -0700

    Removing bitcoin authentication file that isn't needed

--- coinauth.py
+++ coinauth.py
@@ -3,7 +3,6 @@
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask.views import MethodView
 from flask.ext.restful import Api, Resource, reqparse, fields, marshal
-import validatebitcoinaddress
 
 app = Flask(__name__, static_url_path = "")
 api = Api(app)
